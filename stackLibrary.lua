146bcd161e8249b4c20712969673a43037e92c45
crispeeweevile <92649061+crispeeweevile@users.noreply.github.com>
2022-06-26 08:44:26 -0500

    fixed that garbage
    Fixed the issue with how the functions were made.
    Added optional stack maxSize

--- stackLibrary.lua
+++ stackLibrary.lua
@@ -1,20 +1,22 @@
 local Stack = {}
 Stack.__index = Stack
 
-Stack.new = function ()
-    local newStack = setmetatable({}, Stack)
+function Stack.new(stackSize)
+    stackSize = stackSize or 0
+    local newStack = setmetatable({["stack"]={}, ["stackSize"]=stackSize}, Stack) -- (0) = no max size
 
     return newStack
 end
 
-Stack.push = function( self, value )
-    self[#self+1] = value
+function Stack:push(value)
+    if self.stackSize <= 0 then assert(#self.stack < self.stackSize, "Attempted to overload stack!") end
+    self.stack[#self.stack+1] = value
 end
 
-Stack.pop = function( self )
-    assert(#self > 0, "Stack underload!")
-    local output = self[#self]
-    self[#self] = nil
+function Stack:pop()
+    assert(#self.stack > 0, "Attempted to pop from empty stack!")
+    local output = self.stack[#self.stack]
+    self.stack[#self.stack] = nil
     return output
 end
 
